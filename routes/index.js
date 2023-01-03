e76e770755252acd71c309f043e48d5a43b96745
watsoncolin <colin@colinwatson.me>
2023-01-03 14:55:09 -0700

    print server side user agent

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const viewOptions = {
+    title: "instabreakout",
+    user_agent: req.headers["user-agent"],
+  };
+  res.render("index", viewOptions);
 });
 
 module.exports = router;
