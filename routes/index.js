fb42671a85888b6b8668f23776fad75cfce655d9
watsoncolin <colin@colinwatson.me>
2023-01-03 15:38:53 -0700

    tweak content disposition

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get("/", function (req, res, next) {
 router.get("/breakout", function (req, res, next) {
   if (req.headers["user-agent"].includes("Instagram")) {
     res.contentType("application/octet-stream");
-    res.set("Content-Disposition", "inline; filename=breakout");
+    res.set("Content-Disposition", "attachment; filename='breakout'");
     res.set("Content-Transfer-Encoding", "binary");
     res.set("Accept-Ranges", "bytes");
     res.end(Buffer.from(new Uint8Array(50)));
